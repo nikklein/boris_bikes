1e4f05e48ac26f32e91194e48e8caca41049d275
nikklein <objcoding@gmail.com>
2016-07-06 17:53:00 +0100

    Add method initialize(no test)

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,8 +3,10 @@ require_relative 'bike'
 class DockingStation
 #raise error if bike.nil?
   attr_reader :bike
-
-  def release_bike
+def initialize
+@bike = 20
+end
+def release_bike
 raise 'No bikes availible' if @bike.nil?
       @bike
   end
