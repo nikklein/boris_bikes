aec8ccd4fa0ab4acbd44da615128c88e9fe9f40f
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-07-07 17:44:52 +0100

    Isolating tests with doubles

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,36 +2,36 @@ require 'docking_station'
 
 describe DockingStation do
 
-
   describe '#release_bike' do
     it {is_expected.to respond_to(:release_bike)}
 
     it 'raises an error if there no bikes' do
       expect{subject.release_bike}.to raise_error 'No bikes availible'
     end
-it 'doesnt release broken bikes' do
-bike = Bike.new
-bike.report_broken
-subject.dock bike
-expect{subject.release_bike}.to raise_error 'Bike is broken'
-end
-    it 'returns docked bikes' do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.bikes).to eq [bike]
+
+    it 'doesnt release broken bikes' do
+      bike = double(:bike)
+      bike.report_broken
+      subject.dock bike
+      expect{subject.release_bike}.to raise_error 'Bike is broken'
     end
   end
 
   describe '#dock' do
-  it {is_expected.to respond_to(:dock).with(1).argument}
+    it {is_expected.to respond_to(:dock).with(1).argument}
 
-  it 'raises an error if full' do
-    subject.capacity.times {subject.dock(Bike.new)}
-    expect{subject.dock(Bike.new)}.to raise_error 'The dock is full'
+    it 'raises an error if full' do
+      subject.capacity.times {subject.dock(double(:bike))}
+      expect{subject.dock(double(:bike))}.to raise_error 'The dock is full'
     end
-  it 'docks something' do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq [bike]
+    it 'docks something' do
+      bike = double(:bike)
+      expect(subject.dock(bike)).to eq [bike]
+    end
+    it 'returns docked bikes' do
+      bike = double(:bike)
+      subject.dock(bike)
+      expect(subject.bikes).to eq [bike]
     end
   end
 
