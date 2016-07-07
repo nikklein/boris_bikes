f1e0e0053833b44c6441c56f41ba7f336137390d
nikklein <objcoding@gmail.com>
2016-07-07 11:15:45 +0100

    Refactoring and add private methods: empty? and full?

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,13 +9,23 @@ class DockingStation
   end
 
   def release_bike
-    raise 'No bikes availible' if @bikes.empty?
+    raise 'No bikes availible' if empty?
     @bikes.pop
   end
 
   def dock bike
-    raise 'The dock is full' if @bikes.count >= 20
+    raise 'The dock is full' if full?
     @bikes << bike
 
   end
+
+private
+
+  def empty?
+    @bikes.empty?
+  end
+  def full?
+    @bikes.count >= 20
+  end
+
 end
