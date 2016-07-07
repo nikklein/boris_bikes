dcf9f84c3e37425fa39da55eb2da69b77d9b9c50
Aga Kowalczuk <kowalczuk.a@outlook.com>
2016-07-07 13:28:52 +0100

    Refactoring docking_station_spec.rb

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,32 +1,33 @@
 require 'docking_station'
 
 describe DockingStation do
-  it {is_expected.to respond_to(:release_bike)}
+
 
   describe '#release_bike' do
+    it {is_expected.to respond_to(:release_bike)}
+
     it 'raises an error if there no bikes' do
       expect{subject.release_bike}.to raise_error 'No bikes availible'
     end
+
+    it 'returns docked bikes' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bikes).to eq [bike]
+    end
   end
 
-  it {is_expected.to respond_to(:dock).with(1).argument}
-
   describe '#dock' do
+  it {is_expected.to respond_to(:dock).with(1).argument}
+  
   it 'raises an error if full' do
     subject.capacity.times {subject.dock(Bike.new)}
     expect{subject.dock(Bike.new)}.to raise_error 'The dock is full'
     end
-  end
-
   it 'docks something' do
     bike = Bike.new
     expect(subject.dock(bike)).to eq [bike]
-  end
-
-  it 'returns docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bikes).to eq [bike]
+    end
   end
 
   it {is_expected.to respond_to(:bikes)}
